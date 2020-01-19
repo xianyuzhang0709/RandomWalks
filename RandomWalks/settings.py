0e4ac5dbae37bddbbc2b80ff24a0a944003fe6aa
Xianyu <zhangxianyu.tech@outlook.com>
2020-01-19 20:24:38 +0100

    try wanqu tempo

--- RandomWalks/settings.py
+++ RandomWalks/settings.py
@@ -14,6 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -69,7 +72,7 @@ ROOT_URLCONF = 'RandomWalks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,6 +80,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -133,3 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
--- RandomWalks/urls.py
+++ RandomWalks/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from webapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('webapp/', include('webapp.urls')),
-    path('', include('webapp.urls')),
+    # path('', include('webapp.urls')),
     path('admin/', admin.site.urls),
-]
+    path('', views.index, name='index'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
